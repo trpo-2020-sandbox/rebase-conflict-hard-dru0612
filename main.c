75abdd4e7bfb1e71643171e0cef0097caac41486
srwxrwxrwx <srwxrwxrwx@users.noreply.github.com>
2017-03-04 17:30:36 +0700

    Count array items with macro

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
+
 int min_element(int* arr, int size)
 {
     int min = *arr;
@@ -18,11 +20,23 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
+<<<<<<< HEAD
     const int min = min_element(arr, 10);
+||||||| constructed merge base
+    const int min = min_element(arr, 7);
+=======
+    const int min = min_element(arr, N_ELEMENTS(arr));
+>>>>>>> Count array items with macro
 
     printf("Array: ");
     int i;
+<<<<<<< HEAD
     for (i = 0; i < 10; ++i) {
+||||||| constructed merge base
+    for (i = 0; i < 7; ++i) {
+=======
+    for (i = 0; i < N_ELEMENTS(arr); ++i) {
+>>>>>>> Count array items with macro
         printf("%d ", arr[i]);
     }
     printf("\n");
