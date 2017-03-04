08f7bee3d57d0eade468958d29891132b9b30540
srwxrwxrwx <srwxrwxrwx@users.noreply.github.com>
2017-03-04 17:43:05 +0700

    Extract printing array to function

--- main.c
+++ main.c
@@ -20,23 +20,11 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
 
-<<<<<<< HEAD
-    const int min = min_element(arr, 10);
-||||||| constructed merge base
-    const int min = min_element(arr, 7);
-=======
     const int min = min_element(arr, N_ELEMENTS(arr));
->>>>>>> Count array items with macro
 
     printf("Array: ");
     int i;
-<<<<<<< HEAD
-    for (i = 0; i < 10; ++i) {
-||||||| constructed merge base
-    for (i = 0; i < 7; ++i) {
-=======
     for (i = 0; i < N_ELEMENTS(arr); ++i) {
->>>>>>> Count array items with macro
         printf("%d ", arr[i]);
     }
     printf("\n");
